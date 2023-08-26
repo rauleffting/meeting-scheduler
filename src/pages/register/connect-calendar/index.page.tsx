31c1a4ebc2f6cf3eb4001388bf19e004180fbfc1
Raul Effting <rauleffting94@gmail.com>
2023-08-26 11:43:45 -0300

    feat: add oauth with next auth and adapter

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,27 +1,31 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
-import { ArrowRight, Check } from 'phosphor-react'
-import { Container, Header } from '../styles'
-import { AuthError, ConnectBox, ConnectItem } from './styles'
 import { signIn, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import { ArrowRight, Check } from 'phosphor-react'
+// import { api } from "../../../lib/axios"
+import { Container, Header } from '../styles'
+import { AuthError, ConnectBox, ConnectItem } from './styles'
 
-export default function ConnectCalendar() {
+export default function Register() {
   const session = useSession()
   const router = useRouter()
+
   const hasAuthError = !!router.query.error
-  const isSignedIn = session.status === 'authenticated'
+  const isSignedId = session.status === 'authenticated'
 
   async function handleConnectCalendar() {
-    await signIn('google', { callbackUrl: '/register/connect-calendar' })
+    await signIn('google')
   }
+
+  console.log(session)
 
   return (
     <Container>
       <Header>
-        <Heading as="strong">Connect your schedule!</Heading>
+        <Heading as="strong">Conecte sua agenda!</Heading>
         <Text>
-          Connect your calendar to automatically check busy hours and new events
-          as they are scheduled.
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
         </Text>
 
         <MultiStep size={4} currentStep={2} />
@@ -30,32 +34,32 @@ export default function ConnectCalendar() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          {!isSignedIn ? (
+          {isSignedId ? (
+            <Button size="sm" disabled>
+              Conectado
+              <Check />
+            </Button>
+          ) : (
             <Button
               variant="secondary"
               size="sm"
               onClick={handleConnectCalendar}
             >
-              Connect
+              Conectar
               <ArrowRight />
-            </Button>
-          ) : (
-            <Button size="sm" disabled>
-              Connected
-              <Check />
             </Button>
           )}
         </ConnectItem>
 
         {hasAuthError && (
           <AuthError size="sm">
-            Failed to connect to Google, make sure you have enabled Google
-            Calendar access permissions.
+            Falha ao se conectar ao Google, verifique se você habilitou as
+            permissões de acesso ao Google Calendar
           </AuthError>
         )}
 
-        <Button type="submit" disabled={!isSignedIn}>
-          Next step
+        <Button type="submit" disabled={!isSignedId}>
+          Próximo passo
           <ArrowRight />
         </Button>
       </ConnectBox>
